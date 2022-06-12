958b3dd91223e435befc398e6af076a7c7cc3406
Edoardo Fullin <edoardo.fullin@outlook.com>
2022-06-12 14:41:43 +0200

    modifiche relazione

--- project/project_reti_logiche.runs/synth_1/project_reti_logiche.tcl
+++ project/project_reti_logiche.runs/synth_1/project_reti_logiche.tcl
@@ -70,11 +70,7 @@ proc create_report { reportName command } {
   }
 }
 OPTRACE "synth_1" START { ROLLUP_AUTO }
-set_param checkpoint.writeSynthRtdsInDcp 1
-set_param synth.incrementalSynthesisCache ./.Xil/Vivado-7264-edoardo-macmi/incrSyn
 set_msg_config -id {Common 17-41} -limit 10000000
-set_msg_config -id {Synth 8-256} -limit 10000
-set_msg_config -id {Synth 8-638} -limit 10000
 OPTRACE "Creating in-memory project" START { }
 create_project -in_memory -part xc7a200tfbg484-1
 
